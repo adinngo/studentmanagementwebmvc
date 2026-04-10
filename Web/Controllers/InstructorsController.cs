a34d7a6dc929b5712db9545a3f3bc7dcfb7759cf
thang ngo <gnahtong123@gmail.com>
2026-04-10 09:28:46 +0700

    add sort, filter and pagination for instructors controller

--- Web/Controllers/InstructorsController.cs
+++ Web/Controllers/InstructorsController.cs
@@ -15,10 +15,25 @@ namespace Web.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(int? id, int? courseID)
+
+        public async Task<IActionResult> Index(int? id, int? courseID, int? pageNumber, string sortOrder, string currentFilter, string searchString)
         {
-            InstructorIndexVM viewModel = new();
-            viewModel.InstructorsVM = await _context.Instructors.Select(p => new InstructorViewModel
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParam = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParam = sortOrder == "Date" ? "date_desc" : "date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var viewModel = new InstructorIndexVM();
+            var instructors = _context.Instructors.Select(p => new InstructorViewModel
             {
                 ID = p.ID,
                 LastName = p.LastName,
@@ -31,7 +46,24 @@ namespace Web.Controllers
                     Title = c.Course.Title
 
                 })
-            }).AsNoTracking().ToListAsync();
+            });
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                instructors = instructors.Where(s => s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));
+            }
+
+            instructors = sortOrder switch
+            {
+                "name_desc" => instructors.OrderByDescending(s => s.LastName),
+                "date_desc" => instructors.OrderByDescending(s => s.HireDate),
+                "date" => instructors.OrderBy(s => s.HireDate),
+                _ => instructors.OrderBy(s => s.LastName),
+            };
+
+            int pageSize = 2;
+            ViewBag.CurrentPage = pageNumber;
+            viewModel.InstructorsVM = await PaginatedList<InstructorViewModel>.CreateAsync(instructors.AsNoTracking(), pageNumber ?? 1, pageSize);
 
             if (id != null)
             {
--- Web/Models/Instructor/InstructorIndexVM.cs
+++ Web/Models/Instructor/InstructorIndexVM.cs
@@ -6,7 +6,7 @@ namespace Web.Models.Instructor
     public class InstructorIndexVM
     {
 
-        public IEnumerable<InstructorViewModel> InstructorsVM { get; set; }
+        public PaginatedList<InstructorViewModel> InstructorsVM { get; set; }
         public IEnumerable<CourseViewModel> CoursesVM { get; set; }
         public IEnumerable<EnrollmentViewModel> EnrollmentsVM { get; set; }
     }
